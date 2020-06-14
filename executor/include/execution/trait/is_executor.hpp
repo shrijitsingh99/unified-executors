be448a9448971a7db8b36fa0bd5bee997cc346d4
Shrijit Singh <shrijitsingh99@gmail.com>
2020-06-15 03:12:12 +0530

    Make `is_executor` permanently true temporarily
    Temporay change till the executor conditions mentioned (http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p1660r0.pdf) are used

--- executor/include/execution/trait/is_executor.hpp
+++ executor/include/execution/trait/is_executor.hpp
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <execution/executor/base_executor.hpp>
 #include <experimental/type_traits>
 #include <type_traits>
 
@@ -15,11 +16,10 @@ struct is_executor : std::false_type {};
 constexpr const auto noop = [] {};
 
 template <class Executor>
-struct is_executor<
-    Executor,
-    std::enable_if_t<
-        std::is_same_v<void, decltype(std::declval<Executor>().execute(noop))>,
-        void>> : std::true_type {};
+struct is_executor<Executor, std::enable_if_t<true, void>> : std::true_type {};
+
+// #TODO: Satisfy executor conditions
+// (http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p1660r0.pdf)
 
 template <typename Executor>
 using is_executor_t = typename is_executor<Executor>::type;
