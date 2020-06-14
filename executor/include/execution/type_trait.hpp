041f2698869e56860b5a853f0b1dc0fb6f867fb2
Shrijit Singh <shrijitsingh99@gmail.com>
2020-06-15 03:20:08 +0530

    Split and rename is_instance_of

--- executor/include/execution/type_trait.hpp
+++ executor/include/execution/type_trait.hpp
@@ -3,17 +3,4 @@
 #include <execution/trait/can_require.hpp>
 #include <execution/trait/is_executor.hpp>
 #include <execution/trait/is_executor_available.hpp>
-
-namespace execution {
-
-template <template <typename...> class Type, typename Executor>
-struct is_instance_of : std::false_type {};
-
-template <template <typename...> class Type, typename... Args>
-struct is_instance_of<Type, Type<Args...>> : std::true_type {};
-
-template <template <typename...> class Type, typename Executor>
-using instance_of =
-    std::enable_if_t<is_instance_of<Type, Executor>::value, int>;
-
-}  // namespace execution
+#include <execution/trait/is_instance_of_base.hpp>
