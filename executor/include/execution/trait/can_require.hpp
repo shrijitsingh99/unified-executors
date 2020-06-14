8d1eac729d2fea026b156fc0d304f91cee39eb6d
Shrijit Singh <shrijitsingh99@gmail.com>
2020-06-14 19:07:34 +0530

    Add require type trait

---
+++ executor/include/execution/trait/can_require.hpp
@@ -0,0 +1,32 @@
+//
+// Created by Shrijit Singh on 14/06/20.
+//
+
+#pragma once
+
+#include <type_traits>
+
+namespace execution {
+
+template <typename Executor, typename Property,
+          typename std::enable_if_t<
+              Property::template is_applicable_property_v<Executor> &&
+                  Property::template static_query<Executor>(),
+              int> = 0>
+Executor require(Executor ex, const Property t) {
+  return ex.require(t);
+}
+
+template <typename Executor, typename Properties, typename = void>
+struct can_require : std::false_type {};
+
+template <typename Executor, typename Property>
+struct can_require<Executor, Property,
+                   std::void_t<decltype(execution::require(
+                       std::declval<Executor>(), std::declval<Property>()))>>
+    : std::true_type {};
+
+template <typename Executor, typename Property>
+constexpr bool can_require_v = can_require<Executor, Property>::value;
+
+}  // namespace execution
