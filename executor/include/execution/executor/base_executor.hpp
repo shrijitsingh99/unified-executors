148e87d6b57858a30c4fbb1d38f225b200f42459
Shrijit Singh <shrijitsingh99@gmail.com>
2020-06-15 03:13:45 +0530

    Add default bulk_execute functionality
    http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p1660r0.pdf
    Seems to indicate, a default bulk_execute will be provided which repeatedly calls execute

--- executor/include/execution/executor/base_executor.hpp
+++ executor/include/execution/executor/base_executor.hpp
@@ -4,20 +4,17 @@
 
 #pragma once
 
-#include <functional>
-#include <iostream>
-#include <stdexcept>
-#include <string>
 #include <array>
-#include <memory>
-
 #include <execution/property.hpp>
 #include <execution/type_trait.hpp>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
 
-
-template <template <typename, typename, typename, typename> typename Derived,
-    typename Interface, typename Cardinality, typename Blocking,
-    typename ProtoAllocator>
+template <template <typename...> typename Derived, typename Interface,
+          typename Cardinality, typename Blocking, typename ProtoAllocator>
 class executor {
  public:
   static constexpr bool query(const blocking_t::always_t &t) {
@@ -38,5 +35,12 @@ class executor {
 
   static constexpr bool query(const twoway_t &t) {
     return std::is_same<Interface, twoway_t>();
+  }
+
+  template <typename F, typename... Args>
+  void bulk_execute(F &&f, Args &&... args, std::size_t n) {
+    for (std::size_t i = 0; i < n; ++i) {
+      std::invoke(std::forward<F>(f), std::forward<Args>(args)..., i);
+    }
   }
 };
--- executor/include/execution/trait/is_executor_available.hpp
+++ executor/include/execution/trait/is_executor_available.hpp
@@ -6,10 +6,10 @@
 
 namespace execution {
 
-template <template <typename, typename, typename, typename> typename T>
+template <template <typename...> typename T>
 struct is_executor_available : std::false_type {};
 
-template <template <typename, typename, typename, typename> typename T>
+template <template <typename...> typename T>
 using is_executor_available_t = typename is_executor_available<T>::type;
 
 }  // namespace execution
