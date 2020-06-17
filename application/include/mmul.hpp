4af1a96336ef14ed2fe73878ef784b19601fc0fc
Shrijit Singh <shrijitsingh99@gmail.com>
2020-06-17 22:33:58 +0530

    Switch to lambdas

--- application/include/mmul.hpp
+++ application/include/mmul.hpp
@@ -17,16 +17,17 @@
 using namespace Eigen;
 
 template <typename Executor,
-          typename execution::instance_of<inline_executor, Executor> = 0>
+          typename execution::instance_of_base<inline_executor, Executor> = 0>
 void mmul(Executor ex, MatrixXd &a, MatrixXd &b, MatrixXd &c) {
-  c = a * b;
+  auto mul = [](MatrixXd &a, MatrixXd &b, MatrixXd &c) { c = a * b; };
+
+  ex.execute(std::bind(mul, std::ref(a), std::ref(b), std::ref(c)));
 }
 
 template <typename Executor,
-          typename execution::instance_of<omp_executor, Executor> = 0>
+          typename execution::instance_of_base<omp_executor, Executor> = 0>
 void mmul(Executor ex, MatrixXd &a, MatrixXd &b, MatrixXd &c) {
-  auto mul = [=](MatrixXd &a, MatrixXd &b, MatrixXd &c,
-                 std::size_t thread_idx) {
+  auto mul = [](MatrixXd &a, MatrixXd &b, MatrixXd &c, std::size_t thread_idx) {
 #pragma omp for schedule(static)
     for (int i = 0; i < a.rows(); i = i + 1) {
       for (int j = 0; j < b.cols(); j = j + 1) {
@@ -37,16 +38,21 @@ void mmul(Executor ex, MatrixXd &a, MatrixXd &b, MatrixXd &c) {
       }
     }
   };
-  ex.bulk_execute(mul, 1, a, b, c);
+  ex.bulk_execute(std::bind(mul, std::ref(a), std::ref(b), std::ref(c),
+                            std::placeholders::_1),
+                  a.rows());
 }
 
 template <typename Executor,
-          typename execution::instance_of<cuda_executor, Executor> = 0>
+          typename execution::instance_of_base<cuda_executor, Executor> = 0>
 void mmul(Executor ex, MatrixXd &a, MatrixXd &b, MatrixXd &c) {
-  void *a_g, *b_g, *c_g;
-  a_g = device_upload(static_cast<void *>(a.data()), a.size() * sizeof(double));
-  b_g = device_upload(static_cast<void *>(b.data()), b.size() * sizeof(double));
-  c_g = device_upload(static_cast<void *>(c.data()), c.size() * sizeof(double));
+  double *a_g, *b_g, *c_g;
+  a_g = static_cast<double *>(
+      device_upload(static_cast<void *>(a.data()), a.size() * sizeof(double)));
+  b_g = static_cast<double *>(
+      device_upload(static_cast<void *>(b.data()), b.size() * sizeof(double)));
+  c_g = static_cast<double *>(
+      device_upload(static_cast<void *>(c.data()), c.size() * sizeof(double)));
 
   std::array<int, 6> shape{static_cast<int>(ceil(a.rows() / 2.0)),
                            static_cast<int>(ceil(b.cols() / 2.0)),
@@ -56,7 +62,10 @@ void mmul(Executor ex, MatrixXd &a, MatrixXd &b, MatrixXd &c) {
                            1};
 
   cuda_executor<oneway_t, bulk_t, blocking_t::always_t, void>{}.bulk_execute(
-      mmul_gpu, shape, a_g, b_g, c_g, a.rows(), a.cols(), b.cols());
+      [=](int stream_id) {
+        mmul_gpu(a_g, b_g, c_g, a.rows(), a.cols(), b.cols());
+      },
+      shape);
 
   memcpy(static_cast<double *>(c.data()), c_g, 9 * sizeof(double));
 }
