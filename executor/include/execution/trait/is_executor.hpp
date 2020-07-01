fe249605c8ba21fd1ea17b7132f5d246605778c6
Shrijit Singh <shrijitsingh99@gmail.com>
2020-07-02 00:18:29 +0530

    Cleanup  `is_executor` trait

--- executor/include/execution/trait/is_executor.hpp
+++ executor/include/execution/trait/is_executor.hpp
@@ -5,21 +5,22 @@
 #pragma once
 
 #include <execution/executor/base_executor.hpp>
+#include <execution/trait/common_traits.hpp>
 #include <experimental/type_traits>
 #include <type_traits>
 
 namespace execution {
 
-template <class Executor, typename Function = void (*)(), typename = void>
+template <class Executor, typename = void>
 struct is_executor : std::false_type {};
 
 namespace detail {
 const auto noop = [] {};
 }
 
-template <typename Executor, typename Function>
+template <typename Executor>
 struct is_executor<
-    Executor, Function,
+    Executor,
     std::enable_if_t<
         std::is_copy_constructible<
             std::remove_cv_t<std::remove_reference_t<Executor>>>::value &&
@@ -27,8 +28,9 @@ struct is_executor<
                                       std::remove_reference_t<Executor>>>()
                                       .execute(detail::noop)),
                          void>::value,
-        decltype(std::declval<Executor>() == std::declval<Executor>(),
-                 (void)0)>> : std::true_type {};
+        COMMON_TRAIT_NS::void_t<decltype(std::declval<Executor>() ==
+                                         std::declval<Executor>())>>>
+    : std::true_type {};
 
 template <typename Executor>
 constexpr bool is_executor_v = is_executor<Executor>::value;
